b0130ac7adb62e787240c6d3d16df94edb30095e
vladiere <ruthakaza@gmail.com>
2023-10-16 12:02:38 +0800

    initial commit

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ import authorRoute from "./routes/author.route";
 import publisherRoute from "./routes/publisher.route";
 import librarianRoute from "./routes/librarian.route";
 import refreshTokenRoute from "./routes/refreshToken.route";
-import userRoute from "./routes/user.route";
+import userRoute from './routes/user.route'
 
 const app: Express = express();
 const PORT: number = Number(process.env.PORT) || 3000;
@@ -18,9 +18,9 @@ const PORT: number = Number(process.env.PORT) || 3000;
 // Logging
 app.use(morgan("dev"));
 
-app.use(cors({ origin: '*'}));
 // Parse the request
 app.use(express.urlencoded({ extended: false }));
+app.use(cors());
 
 // Takes care of json data
 app.use(express.json({ limit: "50mb" })); // Setting the data size of an json
@@ -28,22 +28,22 @@ app.use(bodyParser.json());
 
 app.use(express.static(path.join(__dirname, "public")));
 
-// // Setting the cors policy
-// app.use((req, res, next) => {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept, Authorization",
-//   );
-//
-//   // Setting the cors method headers
-//   if (req.method === "OPTIONS") {
-//     res.header("Access-Control-Allow-Methods", "GET, PATCH, DELETE, POST");
-//     return res.status(200).json({});
-//   }
-//
-//   next();
-// });
+// Setting the cors policy
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization",
+  );
+
+  // Setting the cors method headers
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "GET, PATCH, DELETE, POST");
+    return res.status(200).json({});
+  }
+
+  next();
+});
 
 // Routes here
 app.use("/api/", bookRoutes);
