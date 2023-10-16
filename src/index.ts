97826c1cce7c1f2074f227f1ec5a2f2837793665
vladiere <ruthakaza@gmail.com>
2023-10-16 11:34:39 +0800

    initial commit

--- src/index.ts
+++ src/index.ts
@@ -28,22 +28,22 @@ app.use(bodyParser.json());
 
 app.use(express.static(path.join(__dirname, "public")));
 
-// Setting the cors policy
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization",
-  );
-
-  // Setting the cors method headers
-  if (req.method === "OPTIONS") {
-    res.header("Access-Control-Allow-Methods", "GET, PATCH, DELETE, POST");
-    return res.status(200).json({});
-  }
-
-  next();
-});
+// // Setting the cors policy
+// app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept, Authorization",
+//   );
+//
+//   // Setting the cors method headers
+//   if (req.method === "OPTIONS") {
+//     res.header("Access-Control-Allow-Methods", "GET, PATCH, DELETE, POST");
+//     return res.status(200).json({});
+//   }
+//
+//   next();
+// });
 
 // Routes here
 app.use("/api/", bookRoutes);
